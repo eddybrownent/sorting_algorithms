b467618d3c57f3739446ebc5b51c6f6cd8bb743a
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-06-23 12:23:26 +0300

    error handling enhancing

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,6 +11,9 @@ void selection_sort(int *array, size_t size)
 	int temp;
 	size_t i, j;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = i + 1; j < size; j++)
