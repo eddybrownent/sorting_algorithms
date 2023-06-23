cbc03427c6685c897c2515368ac2217d42447e4a
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-06-23 13:16:10 +0300

    unction that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,22 +9,27 @@
 void selection_sort(int *array, size_t size)
 {
 	int temp;
-	size_t i, j;
+	size_t i, j, min_index;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		min_index = i;
+
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
+			if (array[j] < array[min_index])
+				min_index = j;
 		}
-	print_array(array, size);
+
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
 	}
 }
