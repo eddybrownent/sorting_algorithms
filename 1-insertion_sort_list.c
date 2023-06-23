3549077b7bcd5b79120dcae44b222f1133d47897
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-06-23 11:38:48 +0300

    Sorting algorithms & Big O

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+/**
+ * swap_nodes - swaps two nodes in a listint_t doubly linked list
+ * @list : this is a ponter to head of the list
+ * @node_1: pointer to the first node to swap
+ * @node_2: pointer to the second node to swap
+ */
+void swap_nodes(listint_t **list, listint_t *node_1, listint_t *node_2)
+{
+	if (node_1->prev != NULL)
+		node_1->prev->next = node_2;
+	else
+		*list = node_2;
+
+	if (node_2->next != NULL)
+		node_2->next->prev = node_1;
+
+	node_1->next = node_2->next;
+	node_2->prev = node_1->prev;
+	node_2->next = node_1;
+	node_1->prev = node_2;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ *			using the insertion sort algorithm
+ * @list: pointer to the haed of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *previuos, *current;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		previous = current->prev;
+
+		while (insert != NULL && current->n < insert->n)
+		{
+			swap_nodes(list, insert, current);
+			print_list((const listint_t *)*list);
+			previous = current->prev;
+		}
+	current = current->next;
+	}
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * selection_sort - this function sorts an array of integers
+ *			using selction sort algorithm
+ * @array: points to the array of integers
+ * @size: the size of the array
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[i] > array[j])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	print_array(array, size);
+	}
+}
